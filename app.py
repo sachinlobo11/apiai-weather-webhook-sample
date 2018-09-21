dd7fcd2d0aeb0a8fa9e282210de91b7307d31713
sachinlobo11 <30945603+sachinlobo11@users.noreply.github.com>
2018-09-21 10:48:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-#!/usr/bin/env python
+ you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import urllib
+
+
+from __future__ import print_function
+#from future.standard_library import install_aliases
+#install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
-import sys
 
 from flask import Flask
 from flask import request
@@ -18,95 +35,74 @@ def webhook():
     req = request.get_json(silent=True, force=True)
 
     print("Request:")
+    # commented out by Naresh
     print(json.dumps(req, indent=4))
-    
+
     res = processRequest(req)
-    print("Request processed")
-    
+
     res = json.dumps(res, indent=4)
-    print(res)
-    
+    # print(res)
     r = make_response(res)
-    print(r)
-    
     r.headers['Content-Type'] = 'application/json'
     return r
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "createCheckBook":
+    print ("starting processRequest...",req.get("result").get("action"))
+    if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://ldcif6u.wdf.sap.corp:44304/sap/opu/odata/sap/ZIVRC_SRV/WorkItems("
-    print(baseurl)
-    
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
-    print("yql_query1:")
-    print(yql_query)
-    
     if yql_query is None:
         return {}
-    yql_url = baseurl + urllib.urlencode({yql_query}) + "?$format=json"
-    print(yql_query)
-    
-    result = urllib.urlopen(baseurl).read()
-    print(result)
-    data = json.loads(result)
-    print("data1:")
-    print(data)
-    
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    #data = json.loads(result)
+    #for some the line above gives an error and hence decoding to utf-8 might help
+    data = json.loads(result.decode('utf-8'))
     res = makeWebhookResult(data)
     return res
 
 
 def makeYqlQuery(req):
     result = req.get("result")
-    print("result1:")
-    print(result)
     parameters = result.get("parameters")
-    print("parameters1:")
-    print(parameters)
-    city = parameters.get("workitemtype")
-    print("City1:")
-    print(city)
+    city = parameters.get("geo-city")
     if city is None:
         return None
-        
-        return "guid'" + "0005EEE4-48CC-1ED5-B0C9-FA163EA701AC" + "')"
-   #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
-    print("MakeWebhook method")
-    query = data.get('d')
-    print("Query1:")
-    print(query)
+    print ("starting makeWebhookResult...")
+    query = data.get('query')
     if query is None:
         return {}
 
-    result = query.get('WORKITEM_ID')
+    result = query.get('results')
     if result is None:
         return {}
 
-    channel = query.get('DESCRIPTION')
+    channel = result.get('channel')
     if channel is None:
         return {}
 
-   # item = channel.get('item')
-   # location = channel.get('location')
-   # units = channel.get('units')
-   # if (location is None) or (item is None) or (units is None):
-   #    return {}
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
 
-   # condition = item.get('condition')
-   # if condition is None:
-   #     return {}
+    condition = item.get('condition')
+    if condition is None:
+        return {}
 
     # print(json.dumps(item, indent=4))
 
-   # speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-   #         ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-	speech = " The Work Item No. " + result + " has been created for " + channel
-    print("Response1:")
+    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
     print(speech)
 
     return {
@@ -118,9 +114,32 @@ def makeWebhookResult(data):
     }
 
 
+@app.route('/test', methods=['GET'])
+def test():
+    return  "Hello there my friend !!"
+
+
+@app.route('/static_reply', methods=['POST'])
+def static_reply():
+    speech = "Hello there, this reply is from the webhook !! "
+    my_result =  {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+    res = json.dumps(my_result, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
+    print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
+
