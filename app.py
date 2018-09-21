caf10af477e7119c90c8cceb6d3940c417a9e09e
sachinlobo11 <30945603+sachinlobo11@users.noreply.github.com>
2018-09-21 10:49:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- you may not use this file except in compliance with the License.
+ #you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,7 +11,7 @@
 
 
 
-from __future__ import print_function
+#from __future__ import print_function
 #from future.standard_library import install_aliases
 #install_aliases()
 
